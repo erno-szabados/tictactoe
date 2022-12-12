2e06b0593c4bdb37e35e207c9324bb3e2cf8960d
Erno Szabados <erno.szabados@windowslive.com>
2022-12-12 21:25:06 +0100

    weight calculation and random game.

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,6 @@
+import random
+
+
 # Tic-tac-toe is a classic game that has been around for centuries. 
 # The rules are simple: two players take turns placing either an X or an O on a 3x3 grid, 
 # with the goal of getting three of their marks in a row (vertically, horizontally, or diagonally). 
@@ -30,6 +33,21 @@
 # 3 2 3
 # 2 4 2
 # 3 2 3
+# row evaluation:
+# for row evaluation, store the number of player pieces, opponent pieces, and empty spaces in the row.
+# number of player pieces and opponent pieces can be summed if they have the opposite sign.
+# the number of empty spaces is used to make a decision for identic row values for the next cases:
+# if the player has 1 piece and the opponent has 1 piece, the value is 0, but the zeroes value is 1. 
+# if the row is empty, the value is 0, but the zeroes value is 3.
+# row values are as follow:
+# the row has three opponent pieces, the value is -3, and it is a loss.
+# the row has three player pieces, the value is 3, and it is a win.
+# the row has two opponent pieces, the value is -2.
+# the row has two player pieces, the value is 2.
+# the row has one opponent piece, the value is -1.
+# the row has one player piece, the value is 1.
+# the row has two opponent pieces and a player piece, the value is -1. 
+# the row has two player pieces and an opponent piece, the value is 1.
 
 
 print("tic-tac-toe")
@@ -43,18 +61,65 @@ class tictactoe:
         self.EMPTY = 0
 
         # chosen for summing in win detection
-        self.HUMAN = 1
-        self.COMPUTER = -1
+        self.PLAYER1 = 1
+        self.PLAYER2 = -1
+        self.NAMES = {self.PLAYER1: 'Player 1', self.PLAYER2: 'Player 2'}
 
-        # precalculated board indices to check on win condition
-        self.WIN = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        # precalculated board indices for rows, columns, and diagonals
+        self.TRIPLETS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        # weights for triplet evaluation
+        self.WEIGHTS = self.weights()
 
-    
+
+    def weights(self):
+        '''
+        Calculate row, column, and diagonal (triplet) weights.
+        weight is calculated as follows:
+        if the triplet is empty or full, but not winning or losing, the weight is 0.
+        if the triplet is winning, the weight is 64. if the triplet is losing, the weight is -64.
+        otherwise the weight is the sum of the triplet values multiplied by 16.
+        return a list of tuples with the following format:
+        (triplet, weight)
+        '''
+        values = [-1,0,1]
+        result = []
+        for i in values:
+            for j in values:
+                for k in values:
+                    v = [i,j,k]
+                    z = v.count( 0)
+                    # assume normal weighed condition
+                    w = sum(v) * 16
+                    # neutral triplet or win condition?
+                    if z == 0: 
+                        if abs(sum(v)) < 3:
+                            w = 0 
+                        else:
+                            w = sum(v) * 64
+                    result.append((v,w))
+        return result
+
+    def eval_table_state(self, board):
+        # decompose board into triplets
+        triplets = [[board[i] for i in t] 
+                    for t in self.TRIPLETS]
+        # calculate board triplet weights using precalculated weights
+        #wl = [w for t in triplets for v, w in self.WEIGHTS if v == t]
+        weights = sum(w for t in triplets for v, w in self.WEIGHTS if v == t)
+        #print(triplets)
+        #print(wl)
+        print(weights)
+        return weights
+
+       
     def same_sign(self, n1, n2):
         return (n1 * n2) > 0
 
     def haswon(self, player_id):
-        return any(abs(t) == 3 and self.same_sign(t, player_id) for t in [sum([self.board[i] for i in w]) for w in self.WIN])
+        return any(abs(t) == 3 and self.same_sign(t, player_id) for t in [sum([self.board[i] for i in w]) for w in self.TRIPLETS])
+
+    def is_over(self):
+        return self.haswon(self.PLAYER1) or self.haswon(self.PLAYER2) or self.board.count(self.EMPTY) == 0
 
     def move(self, player_id, x, y):
         ''' 
@@ -78,9 +143,27 @@ class tictactoe:
                 print('--+---+--')
 
 
+def random_moves(game, player_id):
+    x, y = random.randint(0,2), random.randint(0,2)
+    if (game.is_over()):
+        return
+    pre_state = game.eval_table_state(game.board) * player_id
+    while game.move(player_id, x, y) != True:
+        x, y = random.randint(0,2), random.randint(0,2)
+    post_state = game.eval_table_state(game.board) * player_id
+    print("%s move: (%d,%d)" % (game.NAMES[player_id], x, y))
+
+random.seed()
 game = tictactoe()
 game.print_board()
-for i in range(3):
-    print(game.move(game.HUMAN, i,i))
+while game.is_over() == False:
+    random_moves(game, game.PLAYER1)
+    random_moves(game, game.PLAYER2)
     game.print_board()
-print(game.haswon(game.HUMAN))
+if (game.haswon(game.PLAYER1)):
+    print("Player1 won")
+elif (game.haswon(game.PLAYER2)):
+    print("Player2 won")
+else:
+    print("Draw")
+#print(game.eval_table_state(game.board))
